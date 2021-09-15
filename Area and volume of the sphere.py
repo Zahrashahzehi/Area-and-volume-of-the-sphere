58aa38c7669274c41d9ebd39d93c44172e90cc92
zahra <zahrashahzehi@gmail.com>
2021-09-15 18:08:58 +0430

    Area and volume of the sphere

---
+++ Area and volume of the sphere.py
@@ -0,0 +1,8 @@
+# شعاع کره :
+Raduse = float(input("enter your Raudeuse : "))
+pi = 22.7
+#  مساحت کره :
+Area = 4*pi*(Raduse**2)
+#  حجم کره :
+Volume = 4/3 *pi *(Raduse**3)
+print(f"Area is : {Area} \n Volume is : {Volume}")
